837460f787fa9fff173a919cf9a304246f2f134b
polirs <poli.schnornberger@gmail.com>
2017-02-19 16:06:56 -0300

    update

---
+++ Vue/js/app.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+	el: '#app',
+	data: {
+		message: 'Hello Vue!',
+		coisas: ["agua", "suco"]
+	},
+	methods: {
+		teste1: function(){			
+			this.coisas.push(this.message);			
+		}
+	}
+})
