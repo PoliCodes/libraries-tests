f6c4bfec80f32351dc6b9f7c8231a3b9cba2e790
polirs <poli.schnornberger@gmail.com>
2017-03-03 00:55:21 -0300

    mudancas para typescript

--- React/js/app.js
+++ React/js/app.js
@@ -1,5 +1,63 @@
+var dados = {
+	message: "ok"
+}
+
+var Form2 = React.createClass({displayName: "Form2",
+  getInitialState: function() {
+    return dados;
+  },
+  handleChange: function(newValue) {
+    this.setState({message: newValue});
+  },
+  render: function() {
+    var valueLink = {
+      value: this.state.message,
+      requestChange: this.handleChange
+    };
+    return React.createElement("input", {type: "text", valueLink: valueLink});
+  }
+});
+
+
+class Form1 extends React.Component {
+
+	// get initial state
+  getInitialState() {
+    return {message: 'Hello!'};
+  }
+  render() {
+    return (
+      React.createElement("div", {className: "form1"}, "ms", this.message, 
+				React.createElement("input", {value: "this.nome"})
+      )
+    )
+  };
+};
+
+
+class ShoppingList extends React.Component {
+  render() {
+    return (
+      React.createElement("div", {class: "shopping-list"}, 
+        React.createElement("h1", null, "Shopping List for ", this.props.name), 
+        React.createElement("ul", null, 
+          React.createElement("li", null, "Instagram"), 
+          React.createElement("li", null, "WhatsApp"), 
+          React.createElement("li", null, "Oculus")
+        )
+      )
+    )
+  };
+};
+
 ReactDOM.render(
-  React.createElement("h1", null, "222")
-  ,
+	React.createElement("div", null, 
+		React.createElement(Form2, null), 
+		React.createElement(ShoppingList, {name: "Danilo"})
+	)
+	,
   document.getElementById('root')
 );
+
+
+
---
+++ React/js/app2.ts
@@ -0,0 +1,9 @@
+
+ReactDOM.render(
+	<div>
+		<Form2 />
+		<ShoppingList name="Danilo" />
+	</div>
+	,
+  document.getElementById('root')
+);
