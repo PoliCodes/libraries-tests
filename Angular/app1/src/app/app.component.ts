4454d5d522d645b51aa2f541a4347474ad6693ab
polirs <poli.schnornberger@gmail.com>
2017-03-10 08:15:13 -0300

    Angular/Vue TODO LIST OK

--- Angular/app1/src/app/app.component.ts
+++ Angular/app1/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
+@Component({selector: 'app-root', templateUrl: './app.component.html', styleUrls: ['./app.component.css']})
 export class AppComponent {
-  title = 'app works!';
+  item = '';
+  lista = [];
+  disabled = false;
+  insertItem() {
+    this.lista.push(this.item);
+    this.item = "";
+  }
+  deleteItem(item) {    
+    this.lista.splice(item, 1);    
+  }
+  onKey(event : any) { // without type info
+    this.item = event.target.value;
+  }
 }
